ad752d5d76ee75ec5eb02a5f6d48754a606a07a9
Biasiolo <biasiolorafael@gmail.com>
2023-12-20 17:33:17 -0300

    ajuste

--- script.js
+++ script.js
@@ -1,23 +1,26 @@
 $(document).ready(function() {
+    // Variable declarations
     var height = window.innerHeight,
-      x = 0, y = height / 2,
-      curveX = 10,
-      curveY = 0,
-      targetX = 0,
-      xitteration = 0,
-      yitteration = 0,
-      menuExpanded = false;
+        x = 0, y = height / 2,
+        curveX = 10,
+        curveY = 0,
+        targetX = 0,
+        xitteration = 0,
+        yitteration = 0,
+        menuExpanded = false;
   
-    blob = $('#blob'),
-      blobPath = $('#blob-path'),
+    // DOM element references
+    var blob = $('#blob'),
+        blobPath = $('#blob-path'),
+        hamburger = $('.hamburger');
   
-      hamburger = $('.hamburger');
-  
+    // Event handler for mouse movement
     $(this).on('mousemove', function(e) {
       x = e.pageX;
       y = e.pageY;
     });
   
+    // Event handlers for menu interaction
     $('.hamburger, .menu-inner').on('mouseenter', function() {
       $(this).parent().addClass('expanded');
       menuExpanded = true;
@@ -28,13 +31,16 @@ $(document).ready(function() {
       $(this).parent().removeClass('expanded');
     });
   
+    // Easing function for smooth animation
     function easeOutExpo(currentIteration, startValue, changeInValue, totalIterations) {
       return changeInValue * (-Math.pow(2, -10 * currentIteration / totalIterations) + 1) + startValue;
     }
   
+    // Constants for animation
     var hoverZone = 150;
     var expandAmount = 20;
   
+    // Function to create SVG curve animation
     function svgCurve() {
       if ((curveX > x - 1) && (curveX < x + 1)) {
         xitteration = 0;
@@ -59,24 +65,26 @@ $(document).ready(function() {
         yitteration++;
       }
   
+      // Calculate curve values
       curveX = easeOutExpo(xitteration, curveX, targetX - curveX, 100);
       curveY = easeOutExpo(yitteration, curveY, y - curveY, 100);
-  
       var anchorDistance = 200;
       var curviness = anchorDistance - 60;
   
+      // SVG path definition
       var newCurve2 = "M60," + height + "H0V0h60v" + (curveY - anchorDistance) + "c0," + curviness + "," + curveX + "," + curviness + "," + curveX + "," + anchorDistance + "S60," + (curveY) + ",60," + (curveY + (anchorDistance * 2)) + "V" + height + "z";
   
+      // Apply changes to DOM elements
       blobPath.attr('d', newCurve2);
-  
       blob.width(curveX + 60);
-  
       hamburger.css('transform', 'translate(' + curveX + 'px, ' + curveY + 'px)');
-  
       $('h2').css('transform', 'translateY(' + curveY + 'px)');
+  
+      // Continue animation
       window.requestAnimationFrame(svgCurve);
     }
   
+    // Initial call to start animation
     window.requestAnimationFrame(svgCurve);
   });
   
